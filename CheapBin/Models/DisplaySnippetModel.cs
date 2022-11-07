dde7bf21f1a9238b91a3fdc08e8bb185f45fdd72
wertad <pe.ertl@gmail.com>
2022-11-07 11:50:21 +0100

    New button is alive.

---
+++ CheapBin/Models/DisplaySnippetModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CheapBin.Models
+{
+    public class DisplaySnippetModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Title is too long.")]
+        public string Title { get; set; }
+
+
+        public string Snippet { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime ModifyTime { get; set; }
+        public string Author { get; set; }
+    }
+}
